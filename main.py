6309928c2299a8baaa8f753e86a66fbee39506a6
void4 <arndtschnabel@freenet.de>
2021-02-15 21:52:19 +0100

    Add argparse: two command line modes: --watch and manual

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+import argparse
 from time import sleep
 from glob import glob
 
+import PIL
 from PIL import Image
 from watchdog.observers import Observer
 
-# TODO DecompressionBombWarning
+# Have to disable DecompressionBombError limits because these images are large
+PIL.Image.MAX_IMAGE_PIXELS = None
 
 ALLOWED_ENDINGS = ["png", "jpg", "jpeg"]
 
@@ -15,6 +18,16 @@ THUMBFILELIST = "gallery.txt"
 MAX_WIDTH = MAX_HEIGHT = 512
 
 def thumb(path):
+
+	if ".thumb." in path:
+		return
+		
+	if path.rsplit(".", 1)[-1].lower() not in ALLOWED_ENDINGS:
+		return
+		
+	if path.split(os.path.sep)[-2] != "single":
+		return
+
 	print("Resizing", path)
 	img = Image.open(path)
 	img.thumbnail((MAX_WIDTH, MAX_HEIGHT), Image.ANTIALIAS)
@@ -52,22 +65,39 @@ def thumbfilelist():
 class event_handler:
 	@staticmethod
 	def dispatch(event):
-		if event.event_type == "created" and not event.is_directory and ".thumb." not in event.src_path and event.src_path.rsplit(".", 1)[-1].lower() in ALLOWED_ENDINGS and event.src_path.split(os.path.sep)[-2] == "single":
+		if event.event_type == "created" and not event.is_directory:
 			try:
 				thumb(event.src_path)
 				thumbfilelist()
 			except PIL.UnidentifiedImageError as e:
 				print(e)
 
-observer = Observer()
-observer.schedule(event_handler, PATH, recursive=True)
-observer.start()
 
-try:
-	while True:
-		sleep(1)
-finally:
-	observer.stop()
-	observer.join()
+def mode_observe():
+	observer = Observer()
+	observer.schedule(event_handler, PATH, recursive=True)
+	observer.start()
 
+	try:
+		while True:
+			sleep(1)
+	finally:
+		observer.stop()
+		observer.join()
 
+def mode_manual():
+	paths = []
+	for ending in ALLOWED_ENDINGS:
+		paths += glob("map/**/*."+ending, recursive=True)
+	
+	for path in paths:
+		thumb(path)
+
+	thumbfilelist()
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser("generate image thumbnails and info")
+	parser.add_argument("--watch", dest="mode", action="store_const", const=mode_observe, default=mode_manual, help="watch")
+	args = parser.parse_args()
+	
+	args.mode()
